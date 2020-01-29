8301a0b47c47909b315b77aa86dc6c95b2c1abd7
raghavbobal1 <raghavbobal786@gmail.com>
2020-01-29 18:39:32 -0500

    finished reverse string

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -18,7 +18,10 @@ public class LambtonStringTools
         String rev = "";
 
         for(int i = s.length() - 1; i >= 0; i--)
-   
+        { rev = rev + s.charAt(i);
+        }
+        return rev;
+    }
 
 
 
